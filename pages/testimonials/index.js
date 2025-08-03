fe98c685ce1b39e97b12fa4c0664f2e2aea8dc71
Chin Seila <chinseila008@gmail.com>
2025-08-03 16:08:07 +0700

    first commit

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,21 @@
+//components
+import TestimonialSlider from '../../components/TestimonialSlider';
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+  <div className="h-full bg-primary/30 py-32 text-center">
+    <div className="container mx-auto h-full flex flex-col justify-center">
+      {/* title */}
+      <h2 className='h2 mb-8 xl:mb-0'>
+        Waht client <span className='text-accent'>say.</span>
+      </h2>
+      {/* silder */}
+      <div>
+        <TestimonialSlider />
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Testimonials;
