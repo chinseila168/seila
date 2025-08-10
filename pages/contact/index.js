ce9ea87629c4ee79240fb315b05368040f4d1836
Chin Seila <chinseila008@gmail.com>
2025-08-10 23:50:13 +0700

    Update contact

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -20,13 +20,19 @@ const Contact = () => {
         <motion.h2 
           variants={fadeIn('up',0.2)} 
           initial='hidden' 
-          animate='show' exit='hidden' 
+          animate='show' 
+          exit='hidden' 
           className='h2 text-center mb-12'
           >
-          Let's <span className='text-accent'>connect.</span>
+          Let&apos;s <span className='text-accent'>connect.</span>
         </motion.h2>
         {/* form */}
-        <form className='flex-1 flex flex-col gap-6 w-full mx-auto'>
+        <motion.form
+          variants={fadeIn('up',0.4)} 
+          initial='hidden' 
+          animate='show' 
+          exit='hidden' 
+          className='flex-1 flex flex-col gap-6 w-full mx-auto'>
           {/* group */}
           <div className='flex gap-x-6 w-full'>
             <input type='text' placeholder='name' className='input'/>
@@ -36,11 +42,11 @@ const Contact = () => {
             <textarea placeholder='message' className='textarea'></textarea>
             <button className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'>
               <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>
-                Let's talk.
+                Let&apos;s talk.
                 <BsArrowRight className='-transalte-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]'/>
               </span>
             </button>
-        </form>
+        </motion.form>
       </div>
     </div>
   </div>
