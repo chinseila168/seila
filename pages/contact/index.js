5c1fe03ec68c061671873c31e78a1bafa4e9baaf
Chin Seila <chinseila008@gmail.com>
2025-08-10 23:43:24 +0700

    Update contact 1

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -11,17 +11,20 @@ import { motion } from 'framer-motion';
 //varints
 import { fadeIn } from '../../variants';
 
-
-
 const Contact = () => {
   return (
   <div className='h-full bg-primary/30'>
     <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
       <div className='flex flex-col w-full max-w-[700px]'>
         {/* text */}
-        <h2 className='h2 text-center mb-12'>
+        <motion.h2 
+          variants={fadeIn('up',0.2)} 
+          initial='hidden' 
+          animate='show' exit='hidden' 
+          className='h2 text-center mb-12'
+          >
           Let's <span className='text-accent'>connect.</span>
-        </h2>
+        </motion.h2>
         {/* form */}
         <form className='flex-1 flex flex-col gap-6 w-full mx-auto'>
           {/* group */}
